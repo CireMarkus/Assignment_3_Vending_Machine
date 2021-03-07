a6444edfc4c20524cac055080f7f60f2294eb234
CireMarkus <Mrkimjarrow95@gmail.com>
2021-03-07 12:38:12 -0600

    Added TODOs for the main method.

--- Assignment_3_Vending_Machine/Assignment_3_Vending_Machine.cpp
+++ Assignment_3_Vending_Machine/Assignment_3_Vending_Machine.cpp
@@ -12,23 +12,13 @@
 using namespace std;
 
 
+//TODO: Create function to calculate change
+
+
 int main()
 {
-	Drink drink1 = Drink(1);
-	Drink drink2 = Drink(2);
-	Drink drink3 = Drink(3);
-	Drink drink4 = Drink(4);
-	Drink drink5 = Drink();
+	//TODO: Create  Menu
 
-
-
-	cout << fixed << setprecision(2);
-
-	cout << "Item Type: " << drink1.getItemType() << "\n" << "Item Price: " << drink1.getItemPrice() << "\n\n\n";
-	cout << "Item Type: " << drink2.getItemType() << "\n" << "Item Price: " << drink2.getItemPrice() << "\n\n\n";
-	cout << "Item Type: " << drink3.getItemType() << "\n" << "Item Price: " << drink3.getItemPrice() << "\n\n\n";
-	cout << "Item Type: " << drink4.getItemType() << "\n" << "Item Price: " << drink4.getItemPrice() << "\n\n\n";
-	cout << "Item Type: " << drink5.getItemType() << "\n" << "Item Price: " << drink5.getItemPrice() << "\n\n\n";
 
 	return 0;
 }
